ba28e6dce867a00a40f0478974a862dd8ed4e35f
Harshitarora1410 <Harshitarora14100@gmail.com>
2025-06-07 04:12:45 +0530

    added my portfolio website

---
+++ python revision/variables.py
@@ -0,0 +1,17 @@
+age = 21
+Age = 22
+AGE = 22
+
+print(age, Age, AGE)
+
+name1 = "harshit"
+name2 = "dhruv"
+name3 = "varun"
+
+print(name1, name2, name3)
+
+marks1 = 7.8
+marks2 = 8.0
+marks3 = 8.2
+
+print(marks1, marks2, marks3)
